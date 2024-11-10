cf61c43af2abe8a9bf85869f4eb083dec7e5d636
LauraEllingson <lellingson@ufl.edu>
2024-11-10 18:59:51 -0500

    control

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,20 @@
 const { Post, Tag } = require('../models')
 
 async function create(req, res, next) {
-  const {title, body, tags} = req.body
+  const { title, body, tags } = req.body
   // TODO: create a new post
   // if there is no title or body, return a 400 status
+  if (!(title && body))
+    return res.status(400).send('must include title and body')
   // omitting tags is OK
   // create a new post using title, body, and tags
+  const post = await Post.create({
+    title,
+    body,
+    tags,
+  });
   // return the new post as json and a 200 status
+  return res.status(200).json(post);
 }
 
 // should render HTML
@@ -16,6 +24,9 @@ async function get(req, res) {
     // TODO: Find a single post
     // find a single post by slug and populate 'tags'
     // you will need to use .lean() or .toObject()
+    const post = await Post.findOne({ slug }).populate('tags').lean();
+
+
     post.createdAt = new Date(post.createdAt).toLocaleString('en-US', {
       month: '2-digit',
       day: '2-digit',
@@ -29,8 +40,8 @@ async function get(req, res) {
       })
       return comment
     })
-    res.render('view-post', {post, isLoggedIn: req.session.isLoggedIn})
-  } catch(err) {
+    res.render('view-post', { post, isLoggedIn: req.session.isLoggedIn })
+  } catch (err) {
     res.status(500).send(err.message)
   }
 }
@@ -41,16 +52,16 @@ async function getAll(req, res) {
     // get by single tag id if included
     const mongoQuery = {}
     if (req.query.tag) {
-      const tagDoc =  await Tag.findOne({name: req.query.tag}).lean()
+      const tagDoc = await Tag.findOne({ name: req.query.tag }).lean()
       if (tagDoc)
-        mongoQuery.tags = {_id: tagDoc._id }
+        mongoQuery.tags = { _id: tagDoc._id }
     }
     const postDocs = await Post
       .find(mongoQuery)
       .populate({
         path: 'tags'
       })
-      .sort({createdAt: 'DESC'})
+      .sort({ createdAt: 'DESC' })
     const posts = postDocs.map(post => {
       post = post.toObject()
       post.createdAt = new Date(post.createdAt).toLocaleString('en-US', {
@@ -66,21 +77,35 @@ async function getAll(req, res) {
       isLoggedIn: req.session.isLoggedIn,
       tag: req.query.tag
     })
-  } catch(err) {
+  } catch (err) {
     res.status(500).send(err.message)
   }
 }
 
 async function update(req, res) {
   try {
-    const {title, body, tags} = req.body
+    const { title, body, tags } = req.body
     const postId = req.params.id
     // TODO: update a post
     // if there is no title or body, return a 400 status
+    if (!(title && body))
+      return res.status(400).send('must include title and body')
+
     // omitting tags is OK
     // find and update the post with the title, body, and tags
+    const updatePost = await Post.findByIdAndUpdate(
+      postId,
+      {
+        title,
+        body,
+        tags,
+      },
+      { new: true }
+    );
     // return the updated post as json
-  } catch(err) {
+    return res.status(200).json(updatePost);
+  }
+  catch (err) {
     res.status(500).send(err.message)
   }
 }
@@ -89,7 +114,10 @@ async function remove(req, res, next) {
   const postId = req.params.id
   // TODO: Delete a post
   // delete post by id, return a 200 status
+  await Post.findByIdAndDelete(postId);
+  res.status(200).send('Post deleted');
 }
+
 
 module.exports = {
   get,
